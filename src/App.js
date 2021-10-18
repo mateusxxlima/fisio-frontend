2a2a647a6d7bb6e29ea0fc3e4041cfa00133d505
Mateus Lima <mateus.lima@compasso.com.br>
2021-10-18 16:33:28 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+import Routes from './routes/routes';
+import history from './history';
+
+import { AuthProvider } from './contexts/AuthContext';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
